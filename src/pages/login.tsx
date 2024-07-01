082f9276b9f070bdca5a70ad168a378febba5f55
corn-flour <26082352+corn-flour@users.noreply.github.com>
2024-07-01 11:46:28 -0700

    slightly better login form for now

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,60 +1,98 @@
 import { useSession } from "@/components/sessions"
 import { Button } from "@/components/ui/button"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { useRouter } from "next/router"
 import { useState } from "react"
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+const loginSchema = z.object({
+	service: z.string().url(),
+	identifier: z.string(),
+	password: z.string(),
+})
+
+type LoginFormFields = z.infer<typeof loginSchema>
 
 const LoginPage = () => {
-	const [data, setData] = useState({
-		service: "",
-		identifier: "",
-		password: "",
+	const { login } = useSession()
+	const router = useRouter()
+	const [submissionError, setSubmissionError] = useState("")
+
+	const form = useForm<LoginFormFields>({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			service: "https://bsky.social",
+		},
 	})
 
-	const { login } = useSession()
+	const onSubmit: SubmitHandler<LoginFormFields> = async (data) => {
+		try {
+			await login(data)
+			router.push("/")
+		} catch (e) {
+			setSubmissionError((e as Error).message)
+		}
+	}
 
 	return (
-		<form
-			onSubmit={async (e) => {
-				e.preventDefault()
-				await login(data)
-			}}
-			className="flex flex-col gap-4 p-8"
-		>
-			<input
-				name="service"
-				value={data.service}
-				onChange={(e) =>
-					setData((data) => ({
-						...data,
-						service: e.target.value,
-					}))
-				}
-				className="border"
-			/>
-			<input
-				name="identifier"
-				value={data.identifier}
-				onChange={(e) =>
-					setData((data) => ({
-						...data,
-						identifier: e.target.value,
-					}))
-				}
-				className="border"
-			/>
-			<input
-				type="password"
-				name="password"
-				value={data.password}
-				onChange={(e) =>
-					setData((data) => ({
-						...data,
-						password: e.target.value,
-					}))
-				}
-				className="border"
-			/>
-			<Button type="submit">Login</Button>
-		</form>
+		<div className="grid h-screen place-content-center p-4">
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="w-[80vw] max-w-md space-y-4 rounded-lg border p-8"
+				>
+					<FormField
+						control={form.control}
+						name="service"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Service</FormLabel>
+								<FormControl>
+									<Input {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="identifier"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Identifier</FormLabel>
+								<FormControl>
+									<Input {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Password</FormLabel>
+								<FormControl>
+									<Input {...field} type="password" />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit">Log in</Button>
+				</form>
+			</Form>
+		</div>
 	)
 }
 
